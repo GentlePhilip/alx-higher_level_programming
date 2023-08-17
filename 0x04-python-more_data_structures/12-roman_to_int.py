ccbb647c22350a97507ea38969e2e4e64677663f
GentlePhilip <aphilipnyarko@gmail.com>
2023-08-16 17:29:51 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,7 +10,6 @@ def to_subtract(list_num):
     return (max_list - to_sub)
 
 
-
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
@@ -37,6 +36,5 @@ def roman_to_int(roman_string):
                 last_rom = rom_n.get(ch)
 
     num += to_subtract(list_num)
-
 
     return (num)
